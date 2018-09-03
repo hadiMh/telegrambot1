3bdc26092e492ac17be5bc1e4c5d8085ee01a36d
HadiMh <hadih7899@gmail.com>
2018-09-04 01:40:33 +0430

    a

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
     $connection = mysqli_connect(getenv('BOT1DBSERVER'), getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'));
     if(!$connection) {
-        echo "Database connection failed!";
+        echo "Database connection failed. shit!";
     }
