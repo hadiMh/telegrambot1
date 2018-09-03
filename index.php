b0a88a0f6163c221df17cfdf836ac7eeba2394d3
HadiMh <hadih7899@gmail.com>
2018-09-04 00:35:34 +0430

    changed the token to enviremental variable

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
-	include("Telegram.php");
-   date_default_timezone_set("asia/tehran");
-   // Set the bot TOKEN
-   $bot_id = "561851246:AAEwMFjL_qb1lf5O_e9tmGWnr7AvUdoDiEU";
-   // Instances the class
-   $telegram = new Telegram($bot_id);
-   $text = $telegram->Text(); // متنی که کاربر ارسال میکنه
-   $username = $telegram->Username(); // نام کاربری کاربر
-   $name = $telegram->FirstName();
-   $family = $telegram->LastName();
-   $message_id = $telegram->MessageID(); // هر پیغام در تلگرام یک آیدی یکتا دارد
-   $user_id = $telegram->UserID(); // آیدی یکتای کاربر
-   $chat_id = $telegram->ChatID(); // آیدی مکانی که چت صورت میگیرد، مثل خود بات یا آیدی گروه
-   if(!is_null($text) && !is_null($chat_id)){
-   $content = array('chat_id' => $chat_id, 'text' => $user_id);
-   $telegram->sendMessage($content);
-}
+	ini_set('error_reporting', 'E_ALL');
+ 
+    $botToken = getenv('BOT1TOKEN');
+    $webSite = "https://api.telegram.org/bot" . $botToken;
+     
+    $update = file_get_contents("php://input");
+    $update = json_decode($update, TRUE);
+     
+    $chatId = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+     
+    //CustomKeyBord
+    $option = array(array("salam", "Key1"), array("key2", "key3"));
+    $replyMarkup = array(
+     'keyboard' => $option,
+     'one_time_keyboard' => false,
+     'resize_keyboard' => true,
+     'selective' => true
+    );
+    $encodedMarkup = json_encode($replyMarkup, true);
+     
+    function sendMessage($chatId, $message, $r)
+    {
+     $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&reply_markup=" . $r;
+     file_get_contents($url);
+    }
+     
+    switch ($message) {
+     
+     case "/start":
+     sendMessage($chatId, "شروع می کنیم", $encodedMarkup);
+     break;
+     case "salam":
+     sendMessage($chatId, "salam be ruye mahet", $encodedMarkup);
+     break;
+     default:
+     sendMessage($chatId, "chi migi ??", $encodedMarkup);
+     
+    }
