13ba190fdf69375ce2c2df1f4d975e0f33092fe5
HadiMh <hadih7899@gmail.com>
2018-09-04 11:47:55 +0430

    a

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = mysqli_connect(getenv('BOT1DBSERVER')."/hadidb", getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'));
+    $connection = mysqli_connect(getenv('BOT1DBSERVER'), getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'), '3306');
     // var_dump(function_exists('mysqli_connect'));
     if(!$connection) {
         echo "Database connection failed. shittt!";
