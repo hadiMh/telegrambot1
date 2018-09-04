6bcc21d2c48e34a9fd31fc257f9017c7571a05d9
HadiMh <hadih7899@gmail.com>
2018-09-04 12:02:59 +0430

    a

--- index.php
+++ index.php
@@ -23,14 +23,6 @@
             sendMessage($chatId, "سلام. به ربات ارسال پیام خوش آمدید.\nهر پیامی که به من بفرستید مستقیم به برنامه نویس منتقل میشود و ایشون در اولین فرصت پاسخ شما را میدهند.\nپیام های هود را بفرستید:", $encodedMarkup);
             break;
         case "salam":
-            $query = "INSERT INTO `table1`(`name`, `from_id`, `from_firstname`, `from_username`, `chat_id`, `data`) VALUES ('111','222','333','444','555','666')";
-
-            $result = mysqli_query($connection, $query);
-            if($result) {
-                echo "Record Created";
-            } else {
-                die('Query Failed!');
-            }
             sendMessage($chatId, "salam be ruye mahet", $encodedMarkup);
             break;
         case "/myid":
