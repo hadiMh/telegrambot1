389ad2dc69c4a3489e530c7be51471a335e6c032
HadiMh <hadih7899@gmail.com>
2018-09-05 23:04:41 +0430

    Revert "a"
    This reverts commit e1ce6a9aa7b12b7a97d37b038c8ee0cd64e1cf12.

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     /* connect to the database. (Note: using the enviromental variables) */
-    $connection = mysqli_connect(getenv('BOT1DBSERVER'), getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'), '3307');
+    $connection = mysqli_connect(getenv('BOT1DBSERVER'), getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'), '3306');
     if(!$connection) {
         echo "Database connection failed. shittt!";
     }
