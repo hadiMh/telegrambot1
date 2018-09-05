a41d82709b59f7433124e1fc8806f8018f3b5dd9
HadiMh <hadih7899@gmail.com>
2018-09-05 23:02:37 +0430

    aaabbb

--- functions.php
+++ functions.php
@@ -27,8 +27,8 @@
         $result = mysqli_query($connection, $query);
         $row_cnt = mysqli_num_rows($result);
         if(!$row_cnt){
-            
-        $query = "SELECT * FROM table1 WHERE from_id = $userId ";
+            $query = "INSERT INTO table1 (from_id, from_firstname, from_username, chat_id,game_position, user_answers)";
+            $query .=" VALUES ('$userId', '$name', '$username', '$chatId', 0, '[]')";
             $result = mysqli_query($connection, $query);
             if(!$result) {
                 sendMessage($chatId, "QUERY FAILED: " . mysqli_error($connection) ."\n-- " . $query. " ". mysqli_error(), returnEM(array(array("worked!"))));
