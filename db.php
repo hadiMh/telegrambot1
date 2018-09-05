91ada224082469e594dff4171cc6824cae888aba
HadiMh <hadih7899@gmail.com>
2018-09-05 23:03:27 +0430

    Revert "aaabbb"
    This reverts commit a41d82709b59f7433124e1fc8806f8018f3b5dd9.

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     /* connect to the database. (Note: using the enviromental variables) */
-    $connection = mysqli_connect('www.db4free.net', 'hadimh', 'mhdh1999', 'hadidb', '3306');
+    $connection = mysqli_connect(getenv('BOT1DBSERVER'), getenv('BOT1DBUSERNAME'), getenv('BOT1DBPASSWORD'), getenv('BOT1DBTABLE'), '3306');
     if(!$connection) {
         echo "Database connection failed. shittt!";
     }
