3f3fe899ee636a0b5c5db318d9b04b85a7dfba5f
HadiMh <hadih7899@gmail.com>
2018-09-06 01:07:35 +0430

    aaaa

--- functions.php
+++ functions.php
@@ -139,9 +139,8 @@
         $row = $result->fetch_row();
         $answerJson = $row['user_answers'];
         $answerArray = json_decode($answerJson);
-        sendMessage($chatId, gettype($answerArray), returnEM($buttoms[0]));
-
-        array_push($answerArray, $gamePosition, $answer);
+        sendMessage($chatId, $answerArray, returnEM($buttoms[0]));
+        $answerArray[$gamePosition] = $answer;
         sendMessage($chatId, "111regex captured", returnEM($buttoms[0]));
         $newAnswerJson = json_encode($answerArray);
         sendMessage($chatId, $newAnswerJson, returnEM($buttoms[0]));
