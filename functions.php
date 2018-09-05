83f8beb994ef46eb8304bdfe615190c817175a07
HadiMh <hadih7899@gmail.com>
2018-09-06 04:16:56 +0430

    bugs fixed form the last commit

--- functions.php
+++ functions.php
@@ -238,7 +238,7 @@
             $answerJson = $row['user_answers'];
             $answerArray = json_decode($answerJson, true);
             for($i = 1; $i < $MAXNUMBER+1; $i++) {
-                $score += $marks[$i][(int)$answerArray["$i"]];
+                $score += $marks[$i-1][(int)$answerArray["$i"]];
             }
 
             setTheUserScore($score);
